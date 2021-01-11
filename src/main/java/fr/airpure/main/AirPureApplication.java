5ae989020100d176183492b26b089752508dc7dc
moussa <moussasamb1912@gmail.com>
2021-01-11 10:47:00 +0100

    version3

--- src/main/java/fr/airpure/main/AirPureApplication.java
+++ src/main/java/fr/airpure/main/AirPureApplication.java
@@ -109,11 +109,16 @@ public class AirPureApplication {
 				Class.forName("com.mysql.cj.jdbc.Driver");
 				//
 				cn = DriverManager.getConnection(url, login, password);
+				
+				/////////////////////////////////////
 				//
 				st = cn.createStatement();
-				String sql = "SELECT * FROM commune";
+				String sql = "SELECT C2.id FROM commune as C2 \r\n"
+						+ "INNER JOIN departement ON C2.departement_id = departement.id\r\n"
+						+ "INNER JOIN region ON region.id = departement.region_id\r\n"
+						+ "WHERE region.id = 5";
 				
-				rs = st.executeQuery(sql);
+				//rs = st.executeQuery(sql);
 				
 
 				// log.info("mouss" + obj.toString());
@@ -133,17 +138,17 @@ public class AirPureApplication {
 						System.out.println(m.getProperties().getInseeCom());
 						System.out.println(m.getProperties().getNomCom());
 							//insertion sql Latitude
-						String sql2 = "UPDATE commune " + " SET latitude = '"+ m.getProperties().getxL93() + "' " + " WHERE code_insee='"+ m.getProperties().getInseeCom() + "';";
+						String sql2 = "UPDATE commune as C" + " SET C.latitude = '"+ m.getProperties().getxL93() + "' " + " WHERE C.id IN ( "+ sql + ");";
 						st.executeUpdate(sql2);
 						//insertion sql Longitude
-						String sql3 = "UPDATE commune " + " SET longitude = '"+ m.getProperties().getyL93() + "' " + " WHERE code_insee='"+ m.getProperties().getInseeCom() + "';";
-						st.executeUpdate(sql3);
+						//String sql3 = "UPDATE commune " + " SET longitude = '"+ m.getProperties().getyL93() + "' " + " WHERE code_insee='"+ m.getProperties().getInseeCom() + "';";
+						//st.executeUpdate(sql3);	
 						
-						
+						//String optiUpdate =  "UPDATE commune " + " SET latitude = '"+ m.getProperties().getxL93() + "' " + " WHERE commune.id IN' "+ sql; 
 					}
 				
 				
-				
+		///////////////////////		
 			}
 			catch (SQLException e) {
 				// TODO: handle exception
